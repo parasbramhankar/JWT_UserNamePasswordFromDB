2148fd4e4c072126a0bfeb13aafe20b44dcb3258
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-08 11:12:45 +0530

    commit

---
+++ src/main/java/com/example/JWT_UserNamePasswordFromDB/JwtUserNamePasswordFromDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_UserNamePasswordFromDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserNamePasswordFromDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUserNamePasswordFromDbApplication.class, args);
+	}
+
+}
