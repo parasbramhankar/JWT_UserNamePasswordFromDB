ca9ad664ef9f5a5d168316929497e22e918dd50e
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-08 11:12:27 +0530

    Initial commit

---
+++ src/test/java/com/example/JWT_UserNamePasswordFromDB/JwtUserNamePasswordFromDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWT_UserNamePasswordFromDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtUserNamePasswordFromDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
