709522d53617b9ff70f9f8d7e3e0fdbd693063e5
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-08 12:45:14 +0530

    commit

---
+++ src/main/java/com/example/JWT_UserNamePasswordFromDB/service/AdminService.java
@@ -0,0 +1,20 @@
+package com.example.JWT_UserNamePasswordFromDB.service;
+
+import com.example.JWT_UserNamePasswordFromDB.entity.User;
+import com.example.JWT_UserNamePasswordFromDB.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User>getAllUser(){
+
+        return userRepository.findAll();
+    }
+}
--- src/main/java/com/example/JWT_UserNamePasswordFromDB/service/JwtService.java
+++ src/main/java/com/example/JWT_UserNamePasswordFromDB/service/JwtService.java
@@ -1,5 +1,41 @@
 package com.example.JWT_UserNamePasswordFromDB.service;
 
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
 
+    private static final String SECRET_KEY="parasbramhankarinojri1234456";
+    private final SecretKey key= Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()*1000*30))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails user){
+        String username=extractUsername(token);
+
+        return username.equals(user.getUsername());
+    }
 }
