83319b65c13e38a9326f5c18e0a304232cc6a345
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-08 12:45:02 +0530

    commit

---
+++ src/main/java/com/example/JWT_UserNamePasswordFromDB/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.example.JWT_UserNamePasswordFromDB.controller;
+
+import com.example.JWT_UserNamePasswordFromDB.entity.User;
+import com.example.JWT_UserNamePasswordFromDB.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    AdminService service;
+
+    @GetMapping("/allUsers")
+    public ResponseEntity<List<User>>getAllUser(){
+        return ResponseEntity.ok(service.getAllUser());
+    }
+
+}
