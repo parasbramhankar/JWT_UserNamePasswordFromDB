ab82fc88339ddea41e6a0a76f08927b17edef75c
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-08 11:12:51 +0530

    commit

---
+++ src/main/java/com/example/JWT_UserNamePasswordFromDB/dto/SignInRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.JWT_UserNamePasswordFromDB.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInRequestDTO {
+
+    private String username;
+    private String password;
+
+}
